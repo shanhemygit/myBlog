6af97061580acdfaf4b8b1c23d591ad1628380e9
shanhemygit <1336631634@qq.com>
2020-03-17 14:17:10 +0800

    blogDao.js

--- dao/dbutil.js
+++ dao/dbutil.js
@@ -3,7 +3,7 @@ var mysql = require("mysql");
 function createConnection() {
     return mysql.createConnection({
         port: "3306",
-        host: "127.0.0.1",
+        host: "192.168.1.102",
         password: "123",
         user: "root",
         database: "my_blog"
